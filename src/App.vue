d023da070c235c72618822641977e53938e2fa5f
Greta Masiulyte <gretos.masiulytes@gmail.com>
2022-02-07 22:55:43 +0200

    Reaction timer game With Vue.js

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="startGame" :disabled="isPlaying">PLAY</button>
+
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="gameEnded" :score="score" />
+
+</template> 
+
+<script>
+
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      gameEnded: false,
+      delay: null,
+      score: null,
+    }
+  },
+  methods:{
+    startGame() {
+      this.gameEnded = false;
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+    endGame(reactionTime) {
+      this.isPlaying = false;
+      this.gameEnded = true;
+      this.score = reactionTime;
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
